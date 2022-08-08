01201e6977a0203d01abd1273d208a5bb94e7d4a
Kanatani28 <Kanatani28@users.noreply.github.com>
2022-08-08 02:15:36 +0000

    deploy: 761d2a62b1cf0ef3da3ff3d6ecbbbc0ae4807dd5

---
+++ _next/static/g7SYVRc6-lutsV3W87j7m/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/8034fd035ae25f0f.css","static/chunks/pages/index-092ce3d498e58665.js"],"/_error":["static/chunks/pages/_error-7397496ca01950b1.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB()
